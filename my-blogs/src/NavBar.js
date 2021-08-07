690070ed4a112b3eef4ce6d2ec3d2bd7e09265cc
YaduAcharya <yaduacharya120@gmail.com>
2021-08-07 15:31:56 +0530

    Added css styles
    In a new file and created separate branch named: Adding-styles-using-index.css

--- my-blogs/src/NavBar.js
+++ my-blogs/src/NavBar.js
@@ -4,7 +4,11 @@ const NavBar = () => {
             <h1>The React blogs</h1>
             <div className="links">
                 <a href="/">Home </a>
-                <a href="/create">New Blog </a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:"#f1356d",
+                    borderRadius:'7px'
+                }}>New Blog </a>
             </div>
         </nav>
      );
