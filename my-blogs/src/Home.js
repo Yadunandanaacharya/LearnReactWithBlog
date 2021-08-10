837d9a63b5c35b1518a5c75bd3205024db234dca
YaduAcharya <yaduacharya120@gmail.com>
2021-08-10 07:58:22 +0530

    Working with useState
    To change name, making name reactive

--- my-blogs/src/Home.js
+++ my-blogs/src/Home.js
@@ -1,45 +1,32 @@
+import {useState} from 'react';
+
+
 const Home = () => {
 
+    // let name= 'mario';
+    const [name, setName] =  useState('mario');
+
     const handleClick = () => {
-        console.log("Hi, Yadu!");
+        // name = 'Hanuman';
+        setName('Hanuman');
     }
 
-    const handleNameArgument = (name) => {
-        console.log('Hai' + name  );
-    }
-
-    const usingEvent = (e) => {
-        console.log('Using event here ' , e);
-    }
-
-    const displayButtonEvent = (name,e) => {
-        console.log("We used "+name, e.target);
-    }
+    
 
     return ( 
         <div className="home">
             <h2>Home page </h2>
+
+            <p>{name}</p>
             <button onClick={handleClick}>Click me </button>
-            <br></br>
-            <br></br>
+            
 
-            <button onClick={()=>{
-                console.log('Hi! this is calling anonymous when cliked')
-            }}>Click me for anonymous call </button>
+           
 
-            <br></br>
-            <br></br>
+            
 
-            <button onClick={()=>{
-                handleNameArgument("Yadu ")
-            }}>Hi! Click to know my name</button>
-
-            <br></br> <br></br>
-            <button onClick={usingEvent}> Here click to display event</button>
-
-            <br></br> <br></br>
-            <button onClick={(e)=>{displayButtonEvent('Yadu ',e)}}>Click to display Button event</button>
-            <h2>This is about React </h2>
+            
+            
         </div>
      );
 }
