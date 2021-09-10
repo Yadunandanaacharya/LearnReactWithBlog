ba2dd42e5cf24174d5ea64636e20993df2cde551
YaduAcharya <yaduacharya120@gmail.com>
2021-09-10 15:19:37 +0530

    app.js changes removed href and home.js changes

--- my-blogs/src/Home.js
+++ my-blogs/src/Home.js
@@ -2,13 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('mario'); 
+  // const [name, setName] = useState('mario'); 
 
   const handleDelete = (id) =>{
     const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -16,15 +12,21 @@ const Home = () => {
   }
 
 useEffect(()=>{
-  console.log("use effect ran now");
-  console.log(name)
-},[name]);
+  fetch('http://localhost:3001/blogs')
+    .then(res =>{
+      return res.json();
+    })
+    .then(data =>{
+      console.log(data);
+      setBlogs(data);
+    })
+},[]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs"   handleDelete={handleDelete}/>
-      <button onClick={()=> setName('Sri Ram')}>Change name </button>
-      <p>{ name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs"   handleDelete={handleDelete}/>}
+      {/* <button onClick={()=> setName('Sri Ram')}>Change name </button> */}
+      {/* <p>{ name}</p> */}
     </div>
   );
 }
