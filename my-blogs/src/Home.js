ae603f746ad890bca2749c362878180810bc0e88
YaduAcharya <yaduacharya120@gmail.com>
2021-09-11 12:46:44 +0530

    Catching error when server is not running
    And when server cannot fetch data from some end point these changes done.
    Also if error is there loading disappers.

--- my-blogs/src/Home.js
+++ my-blogs/src/Home.js
@@ -4,27 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsPending] = useState(true); 
+  const [error, setError] = useState(null); 
 
 useEffect(()=>{
   setTimeout(()=>{
   fetch('http://localhost:3001/blogs')
     .then(res =>{
+      // console.log(res);
+      if(!res.ok){
+       throw Error('Could not fetch data for that resource'); 
+      }
       return res.json();
     })
     .then(data =>{
       setBlogs(data);
       setIsPending(false);
+      setError(null);
     })
     .catch(error => {
-      console.log(error.message); 
+      setIsPending(false);
+      setError(error); 
     });
 
-  },5000);
+  },1000);
 
 },[]);
 
   return (
     <div className="home">
+      {error && <div> { error } </div>}
       {isLoading && <div> Loading. . .</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
     </div>
