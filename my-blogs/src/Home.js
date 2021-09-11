0c93207a1b5c3162b9e8fdc0bfdd4ff18f2f830d
YaduAcharya <yaduacharya120@gmail.com>
2021-09-11 15:46:05 +0530

    In different useFetch.js file put codes
    of useeffect and usestate to make it more reusable.

--- my-blogs/src/Home.js
+++ my-blogs/src/Home.js
@@ -1,38 +1,14 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsPending] = useState(true); 
-  const [error, setError] = useState(null); 
-
-useEffect(()=>{
-  setTimeout(()=>{
-  fetch('http://localhost:3001/blogs')
-    .then(res =>{
-      // console.log(res);
-      if(!res.ok){
-       throw Error('Could not fetch data for that resource'); 
-      }
-      return res.json();
-    })
-    .then(data =>{
-      setBlogs(data);
-      setIsPending(false);
-      setError(null);
-    })
-    .catch(error => {
-      setIsPending(false);
-      setError(error); 
-    });
-
-  },1000);
-
-},[]);
+  const {data:blogs, isLoading, error} = useFetch('http://localhost:3001/blogs');
 
   return (
     <div className="home">
       {error && <div> { error } </div>}
+      {isLoading && <div> Loading. . .</div>}
       {isLoading && <div> Loading. . .</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
     </div>
