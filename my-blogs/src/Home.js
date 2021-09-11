efcd7d0673dcf52b524770f23c7f14488089b522
YaduAcharya <yaduacharya120@gmail.com>
2021-09-11 10:32:42 +0530

    Added catch inside useeffect and printing it's message in console.

--- my-blogs/src/Home.js
+++ my-blogs/src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
   // const [name, setName] = useState('mario'); 
 
@@ -19,6 +15,9 @@ useEffect(()=>{
       console.log(data);
       setBlogs(data);
     })
+    .catch(error => {
+      console.log(error.message); 
+    });
 },[]);
 
   return (
