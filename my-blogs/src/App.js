8212066757f4246ac7f38cee854081bfc552a524
YaduAcharya <yaduacharya120@gmail.com>
2021-09-17 20:52:09 +0530

    Using routes

--- my-blogs/src/App.js
+++ my-blogs/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   const title = 'Welcome to react blog';
@@ -9,30 +10,27 @@ function App() {
   // const personInfo = {name:'ram' , age:30}
 
   return (
+    <Router>
     <div className="App"> 
-
     <NavBar></NavBar>
-
       <div className="content">
-
-        <Home></Home>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         {/* you can use either like above or <Home /> */}
-
         <h1> {title} </h1>
-
         <p>Likes {likes} times </p>
-
         {/* <p> {personInfo}  </p> */}
-
         <p>{' string Yadu'}</p>
         <h2>{' string Software developer '}</h2>
-
         {/* <a href="" >{1}</a>
         <p>[1, 2, 4]</p> */}
-
         {/* <a href={link} >Youtube</a> */}
       </div>
     </div>
+    </Router>
   );
 }
 
