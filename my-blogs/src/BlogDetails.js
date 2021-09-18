91d7cc7fb37e6a5c225d546b46b324bda47a3dd6
YaduAcharya <yaduacharya120@gmail.com>
2021-09-18 22:14:17 +0530

    Create BlogDetails.js

---
+++ my-blogs/src/BlogDetails.js
@@ -0,0 +1,49 @@
+// import { useParams } from "react-router-dom";
+// import useFetch from "./useFetch";
+
+// const BlogDetails = () => {
+//     const {id} = useParams();
+//     const {data : blog, isPending, error} = useFetch('http://localhost:3000/blogs/' + id );
+
+//     return ( 
+//         <div className="blog-details">
+//             {/* <h2>Blog details {id}</h2> */}
+//             {isPending && <div>Loading...</div>}
+//             {error && <div>{error}</div>}
+//             {blog && (
+//                 <article>
+//                     <h2>{blog.title}</h2>
+//                     <p>Written by {blog.author}</p>
+//                     <div>{blog.body}</div>
+//                 </article>
+//             ) }
+//         </div>
+//      );
+// }
+ 
+// export default BlogDetails;
+
+
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:3000/blogs/' + id);
+
+  return (
+    <div className="blog-details">
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
+    </div>
+  );
+}
+ 
+export default BlogDetails;
