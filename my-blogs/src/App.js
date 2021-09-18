d32190758bf3963c7333cb06e03319929b323927
YaduAcharya <yaduacharya120@gmail.com>
2021-09-18 14:14:26 +0530

    Using exact to go to specific url only

--- my-blogs/src/App.js
+++ my-blogs/src/App.js
@@ -1,35 +1,25 @@
-import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+
 
 function App() {
-  const title = 'Welcome to react blog';
-  const likes = 50;
-  // const link = 'https://www.youtube.com/';
-  // const personInfo = {name:'ram' , age:30}
-
   return (
     <Router>
-    <div className="App"> 
-    <NavBar></NavBar>
-      <div className="content">
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        {/* you can use either like above or <Home /> */}
-        <h1> {title} </h1>
-        <p>Likes {likes} times </p>
-        {/* <p> {personInfo}  </p> */}
-        <p>{' string Yadu'}</p>
-        <h2>{' string Software developer '}</h2>
-        {/* <a href="" >{1}</a>
-        <p>[1, 2, 4]</p> */}
-        {/* <a href={link} >Youtube</a> */}
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
