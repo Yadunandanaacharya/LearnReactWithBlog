eebf797a54111934283b976e485c8c91971f3fb6
YaduAcharya <yaduacharya120@gmail.com>
2021-09-19 17:50:45 +0530

    Added delte button and handle that
    delte request in blogdetails.js

--- my-blogs/src/BlogDetails.js
+++ my-blogs/src/BlogDetails.js
@@ -24,12 +24,22 @@
 // export default BlogDetails;
 
 
-import { useParams } from "react-router-dom";
+import {useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog,  isLoading,error } = useFetch('http://localhost:3001/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick=()=>{
+        fetch('http://localhost:3001/blogs/'+ blog.id, {
+            method:'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
+
 
   return (
     <div className="blog-details">
@@ -45,7 +55,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
-          
+          <button onClick={handleClick}>Delete blog</button>
         </article>
       )}
     </div>
